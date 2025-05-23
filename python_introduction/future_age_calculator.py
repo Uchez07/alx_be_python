739fbc2aedd3919caffc04b1de3dd1af3a7e5c0c
Gacheru <johngacheru533@gmail.com>
2025-05-23 11:59:38 +0300

    corrections for syntax

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -2,4 +2,4 @@ my_age = int(input("How old are?"))
 constant = 27
 age = my_age + constant
 
-print("In 2050,you will be", age,"years old")
+print("In 2050, you will be", age,"years old.")
--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -2,5 +2,5 @@ hours = 2
 second = 3600
 
 seconds = hours * second
-print(hours,"hours is",seconds,"seconds")
+print(hours,"hour(s) is",seconds,"seconds.")
 
