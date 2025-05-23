ccdde38ced0060149c1cf2f6b203c21287c2084d
Gacheru <johngacheru533@gmail.com>
2025-05-23 11:50:58 +0300

    this my new age, huors and area code

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+my_age = int(input("How old are?"))
+constant = 27
+age = my_age + constant
+
+print("In 2050,you will be", age,"years old")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+hours = 2
+second = 3600
+
+seconds = hours * second
+print(hours,"hours is",seconds,"seconds")
+
