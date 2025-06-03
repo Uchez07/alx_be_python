cecce74248c9a2c6aff60c28b80c8054a1b48f35
Gacheru <johngacheru533@gmail.com>
2025-06-03 10:58:50 +0300

    qwerty2

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 task = input("Enter your task:")
 Priority = input("Priority (high/medium/low):")
-time_bound = input("Is it time-bound? (yes/no)").lower()
+time_bound = input("Is it time-bound? (yes/no):").lower()
 
 
 match Priority:
