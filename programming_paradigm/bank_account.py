5cd2572e4c76fa5d5cd4df1e9d91662231401dfc
Gacheru <johngacheru533@gmail.com>
2025-06-12 12:37:56 +0300

    bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount():
+    def __init__(self, initial_balance=0.0):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+            print(f"Deposited: ${amount:.2f}")
+
+    def withdraw(self, amount):
+        if self.amount <= account_balance:
+            print(f"Withdrew: ${amount}")
+
+        else:
+            prin(f"Insufficient funds.")
+        
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
