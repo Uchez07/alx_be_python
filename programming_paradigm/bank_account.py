be961689600d1dd58c41794997ede7972ee8d33b
Gacheru <johngacheru533@gmail.com>
2025-06-12 13:59:11 +0300

    dfgh

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount():
-    def __init__(self, initial_balance=0.0):
+    def __init__(self, initial_balance=100.0):
         self.account_balance = initial_balance
 
     def deposit(self, amount):
