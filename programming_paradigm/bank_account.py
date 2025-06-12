fabde024cd02bfb666406773d1dbb0ba9d5691c7
Gacheru <johngacheru533@gmail.com>
2025-06-12 14:31:48 +0300

    wer

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,7 +5,7 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.account_balance += amount
-            print(f"Deposited: ${amount}")
+            print(f"Deposited: ${amount: .2f}")
 
     def withdraw(self, amount):
         if self.amount <= account_balance:
